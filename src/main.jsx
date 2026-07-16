65a24748d2f763296cc1348f2f49b17966f19d18
Ogabor Mercy Akpana <ogaborvictorigb@gmail.com>
2026-07-16 04:03:21 +0100

    Fix status bar overlapping app content

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Capacitor } from "@capacitor/core";
+import { StatusBar, Style } from "@capacitor/status-bar";
 import App from "./App.jsx";
 import "./index.css";
+
+if (Capacitor.isNativePlatform()) {
+  StatusBar.setOverlaysWebView({ overlay: false });
+  StatusBar.setBackgroundColor({ color: "#0A0F1E" });
+  StatusBar.setStyle({ style: Style.Dark });
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
